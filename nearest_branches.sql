19ee26665e39bb0296dc948a1debeecd54963b7b
Danial <hadizadeh.d@gmail.com>
2024-05-19 23:19:56 +0330

    Allah os Samad

--- nearest_branches.sql
+++ nearest_branches.sql
@@ -1,13 +1,44 @@
-CREATE PROCEDURE FindNearestBranch
+--CREATE PROCEDURE FindNearestBranch
+--    @City VARCHAR(50)
+--AS
+--BEGIN
+--    SET NOCOUNT ON;
+
+--    IF EXISTS (SELECT 1 FROM branches WHERE city = @City)
+--    BEGIN
+--        SELECT branch_id, branch_name, address, city, state, postal_code, country
+--        FROM branches
+--        WHERE city = @City;
+--    END
+--    ELSE
+--    BEGIN
+--        PRINT 'No branches found in the specified city.';
+--    END
+--END;
+--GO
+
+CREATE VIEW v_branches AS
+SELECT 
+    branch_id, 
+    branch_name, 
+    address, 
+    city, 
+    state, 
+    postal_code, 
+    country
+FROM 
+    branches;
+
+ALTER PROCEDURE FindNearestBranch
     @City VARCHAR(50)
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    IF EXISTS (SELECT 1 FROM branches WHERE city = @City)
+    IF EXISTS (SELECT 1 FROM v_branches WHERE city = @City)
     BEGIN
         SELECT branch_id, branch_name, address, city, state, postal_code, country
-        FROM branches
+        FROM v_branches
         WHERE city = @City;
     END
     ELSE
@@ -16,3 +47,4 @@ BEGIN
     END
 END;
 GO
+
